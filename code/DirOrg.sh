a5a90e7995f9f8f8f5512de640a5fe9addc7a212
mattmattoni <matt.mattoni@temple.edu>
2025-10-31 14:56:49 -0500

    file org

---
+++ code/DirOrg.sh
@@ -0,0 +1,23 @@
+
+PROJECT_DIR = 
+
+FS_DIR = 
+
+FMRI_DIR = 
+
+
+#Remove SUBJ subdir in FS
+cd $FS_DIR
+for dir in REMBRANDT-x-*; do
+    #mv "$dir/$dir/"* "$dir/"
+    rmdir "$dir/$dir"
+done
+
+#Clean Sub IDs in FS
+for dir in REMBRANDT-x-*; do
+  cleandir=$(echo "$dir" | sed -E 's/^REMBRANDT-x-([0-9]+)-.*/\1/')
+  mv "$dir" "$cleandir"
+done
+
+#Clean Sub IDs in functional data
+cd $FMRI_DIR
---
+++ code/ciftify_recon_job.sbatch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH --partition=batch
+#SBATCH --job-name=ciftify_recon
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=10
+#SBATCH --time=4:00:00
+#SBATCH --output=/projects/psych_oajilore_chi/mattonim/lld-sal/logs/recon_%j.log
+#SBATCH --error=/projects/psych_oajilore_chi/mattonim/lld-sal/logs/recon_%j.e
+#SBATCH --mail-user=mattonim@uic.edu
+
+module load singularity
+
+rm -f /projects/psych_oajilore_chi/mattonim/lld-sal/logs/recon_*.*
+
+CIFTIFY_IMG=/projects/psych_oajilore_chi/mattonim/tigrlab_fmriprep_ciftify_latest-2019-08-16-454dd291e09f.simg
+PROJECT_DIR=/projects/psych_oajilore_chi/mattonim/rembrandt
+FS_SITE=REMBRANDT-FS7_v1-Baseline-VUMC 
+SUBJECT_ID=REMBRANDT-x-14180-x-14180a-x-FS7_v1-x-d517c7dd
+
+mkdir -p /projects/psych_oajilore_chi/mattonim/rembrandt/data_hcp
+
+echo "Running ciftify_recon_all for subject: $SUBJECT_ID"
+
+singularity exec --bind "$PROJECT_DIR":/data --bind /projects/psych_oajilore_chi/mattonim/lld-sal:/githome "$CIFTIFY_IMG" \
+  /home/code/ciftify/ciftify/bin/ciftify_recon_all $SUBJECT_ID \
+    --fs-subjects-dir /data/"$FS_SITE"/"$SUBJECT_ID" \
+    --ciftify-work-dir /data/data_hcp \
+    --surf-reg FS \
+    --fs-license /githome/license.txt
+
+echo "Done with $SUBJECT_ID"
