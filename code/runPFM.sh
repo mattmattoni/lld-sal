473c2ee98fa592195349a7e85aae5ba87203f427
mattmattoni <matt.mattoni@temple.edu>
2025-11-10 22:55:47 -0600

    test net assign

--- code/runPFM.sh
+++ code/runPFM.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=128G
-#SBATCH --time=12:00:00
+#SBATCH --time=6:00:00
 #SBATCH --output=/home/mattonim/psych_oajilore_chi_link/mattonim/lld-sal/logs/pfm_%j.log
 #SBATCH --error=/home/mattonim/psych_oajilore_chi_link/mattonim/lld-sal/logs/pfm_%j.err
 
