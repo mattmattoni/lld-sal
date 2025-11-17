8a62bb26f719db63846f6780da12545ce4e9fc6e
mattmattoni <matt.mattoni@temple.edu>
2025-11-17 09:39:13 -0600

    Update runPFM.sh

--- code/runPFM.sh
+++ code/runPFM.sh
@@ -33,8 +33,8 @@ process_subject() {
 export -f process_subject
 
 SUBJECT_LIST=/home/mattonim/psych_oajilore_chi_link/mattonim/lld-sal/logs/sublist.txt
-START_LINE=141
-END_LINE=174
+START_LINE=1
+END_LINE=4
 
 echo "Processing subjects $START_LINE-$END_LINE from subject list"
 
