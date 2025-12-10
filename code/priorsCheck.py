aa38cf832ba02208701cba36621d87d7e600a029
mattmattoni <matt.mattoni@temple.edu>
2025-12-09 19:15:06 -0600

    Update priorsCheck.py

--- code/priorsCheck.py
+++ code/priorsCheck.py
@@ -21,12 +21,10 @@ ref_params = cnc.compute_overlap_with_atlases.DataParams(config_file, salience_f
 
 # Choose atlases to compare against (all in fs_LR_32k space)
 atlas_names = [
-    "EG17",        # Evan Gordon 17 networks
-    "TY7",         # Thomas Yeo 7 networks  
-    "TY17",        # Thomas Yeo 17 networks
-    "AS400Y17",    # Schaefer 400-ROI with Yeo 17 networks
-    "MG360J12",    # Glasser 360-ROI with Ji 12 networks
-    "AS200Y17",    # Schaefer 200-ROI with Yeo 17 networks
+    "XS268_8",        
+    "TY7",           
+    "TY17", 
+    "EG27",       
 ]
 
 # Output directory
