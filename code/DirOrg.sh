04ffc8143c8f1eead296964b503a9a7a2036ed30
mattmattoni <matt.mattoni@temple.edu>
2026-04-27 11:38:22 -0500

    Update DirOrg.sh

--- code/DirOrg.sh
+++ code/DirOrg.sh
@@ -8,6 +8,7 @@ for SITE in "${SITES[@]}"; do
   #FS_DIR=$PROJECT_DIR/Baseline-FS-$SITE
   REST1_DIR=$PROJECT_DIR/Baseline-run1-$SITE
   REST2_DIR=$PROJECT_DIR/Baseline-run2-$SITE
+  TASK_DIR=$PROJECT_DIR/MSIT-Baseline-$SITE
 
   # Remove SUBJ subdir in FS
   #cd "$FS_DIR"
@@ -28,19 +29,30 @@ for SITE in "${SITES[@]}"; do
   #  mv "$dir" "$cleandir"
   #done
 
-  # Clean Sub IDs in REST1
-  cd "$REST1_DIR"
-  for dir in REMBRANDT-x-*; do
-    if [ "$SITE" = "UIC" ]; then
-      cleandir=$(echo "$dir" | sed -E 's/^REMBRANDT-x-3REM([0-9]+)-.*/\1/')
-    else
-      cleandir=$(echo "$dir" | sed -E 's/^REMBRANDT-x-([0-9]+)-.*/\1/')
-    fi
-    mv "$dir" "$cleandir"
-  done
+  ## Clean Sub IDs in REST1
+  #cd "$REST1_DIR"
+  #for dir in REMBRANDT-x-*; do
+  #  if [ "$SITE" = "UIC" ]; then
+  #    cleandir=$(echo "$dir" | sed -E 's/^REMBRANDT-x-3REM([0-9]+)-.*/\1/')
+  #  else
+  #    cleandir=$(echo "$dir" | sed -E 's/^REMBRANDT-x-([0-9]+)-.*/\1/')
+  #  fi
+  #  mv "$dir" "$cleandir"
+  #done
+#
+  ## Clean Sub IDs in REST2
+  #cd "$REST2_DIR"
+  #for dir in REMBRANDT-x-*; do
+  #  if [ "$SITE" = "UIC" ]; then
+  #    cleandir=$(echo "$dir" | sed -E 's/^REMBRANDT-x-3REM([0-9]+)-.*/\1/')
+  #  else
+  #    cleandir=$(echo "$dir" | sed -E 's/^REMBRANDT-x-([0-9]+)-.*/\1/')
+  #  fi
+  #  mv "$dir" "$cleandir"
+  #done
 
-  # Clean Sub IDs in REST2
-  cd "$REST2_DIR"
+    # Clean Sub IDs in REST1
+  cd "$TASK_DIR"
   for dir in REMBRANDT-x-*; do
     if [ "$SITE" = "UIC" ]; then
       cleandir=$(echo "$dir" | sed -E 's/^REMBRANDT-x-3REM([0-9]+)-.*/\1/')
