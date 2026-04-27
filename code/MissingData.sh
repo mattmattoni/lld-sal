097fbd38449435a895a3ad97613cce3fe34746fc
mattmattoni <matt.mattoni@temple.edu>
2026-04-27 16:57:55 -0500

    Update MissingData.sh

--- code/MissingData.sh
+++ code/MissingData.sh
@@ -3,7 +3,7 @@
 DATA_DIR="/home/mattonim/psych_oajilore_chi_link/mattonim/rembrandt/data_hcp"
 OUTPUT="/home/mattonim/psych_oajilore_chi_link/mattonim/lld-sal/logs/data_check.txt"
 
-echo "subject,fsaverage_LR32k,rest-1,rest-2" > $OUTPUT
+echo "subject,fsaverage_LR32k,rest-1,rest-2,msit" > $OUTPUT
 
 for subj in $DATA_DIR/*; do
     SUBJ_ID=$(basename $subj)
@@ -15,8 +15,9 @@ for subj in $DATA_DIR/*; do
     [ -d "$subj/MNINonLinear/fsaverage_LR32k" ] && FS="yes" || FS="no"
     [ -d "$subj/MNINonLinear/Results/rest-1" ] && R1="yes" || R1="no"
     [ -d "$subj/MNINonLinear/Results/rest-2" ] && R2="yes" || R2="no"
+    [ -d "$subj/MNINonLinear/Results/task-MSIT" ] && MSIT="yes" || MSIT="no"
 
-    echo "$SUBJ_ID,$FS,$R1,$R2" >> $OUTPUT
+    echo "$SUBJ_ID,$FS,$R1,$R2,$MSIT" >> $OUTPUT
 done
 
 echo "Results written to $OUTPUT"
