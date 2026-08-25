2bc1745104adcc83a5fd8185da38d6ba621cecc5
mattmattoni <matt.mattoni@temple.edu>
2026-08-25 09:20:05 -0500

    month16

--- code/DirOrg.sh
+++ code/DirOrg.sh
@@ -6,9 +6,9 @@ SITES=(VUMC UPMC UIC)
 for SITE in "${SITES[@]}"; do
 
   #FS_DIR=$PROJECT_DIR/Baseline-FS-$SITE
-  REST1_DIR=$PROJECT_DIR/REST-Month08-run1-$SITE
-  REST2_DIR=$PROJECT_DIR/REST-Month08-run2-$SITE
-  TASK_DIR=$PROJECT_DIR/MSIT-Month08-$SITE
+  REST1_DIR=$PROJECT_DIR/Month16-run1-$SITE
+  REST2_DIR=$PROJECT_DIR/Month16-run2-$SITE
+  TASK_DIR=$PROJECT_DIR/MSIT-Month16-$SITE
 
   ## Remove SUBJ subdir in FS
   #cd "$FS_DIR"
@@ -51,25 +51,25 @@ for SITE in "${SITES[@]}"; do
     mv "$dir" "$cleandir"
   done
   
-  ##Clean task
-  #cd "$TASK_DIR"
-  #for dir in REMBRANDT-x-*; do
-#
-  #  if [ "$SITE" = "UIC" ]; then
-  #    cleandir=$(echo "$dir" | sed -E 's/^REMBRANDT-x-3REM([0-9]+)-.*/\1/')
-  #  else
-  #    cleandir=$(echo "$dir" | sed -E 's/^REMBRANDT-x-([0-9]+)-.*/\1/')
-  #  fi
-#
-  #  nested="$dir/$dir/out/PREPROC"
-#
-  #  if [ -d "$nested" ]; then
-  #    mkdir -p "$cleandir"
-  #    mv "$nested" "$cleandir/"
-  #    rm -rf "$dir"
-  #  else
-  #    echo "Skipping $dir (no PREPROC found)"
-  #  fi
-#
-  #done
+  #Clean task
+  cd "$TASK_DIR"
+  for dir in REMBRANDT-x-*; do
+
+    if [ "$SITE" = "UIC" ]; then
+      cleandir=$(echo "$dir" | sed -E 's/^REMBRANDT-x-3REM([0-9]+)-.*/\1/')
+    else
+      cleandir=$(echo "$dir" | sed -E 's/^REMBRANDT-x-([0-9]+)-.*/\1/')
+    fi
+
+    nested="$dir/$dir/out/PREPROC"
+
+    if [ -d "$nested" ]; then
+      mkdir -p "$cleandir"
+      mv "$nested" "$cleandir/"
+      rm -rf "$dir"
+    else
+      echo "Skipping $dir (no PREPROC found)"
+    fi
+
+  done
 done
