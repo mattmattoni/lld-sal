96ae57b70d011797878ec4cc4f6386e3ac9bab84
mattmattoni <matt.mattoni@temple.edu>
2026-09-02 11:01:20 -0500

    24 prep

--- code/MissingData.sh
+++ code/MissingData.sh
@@ -15,9 +15,9 @@ for subj in $DATA_DIR/*; do
 
     # Check each folder
     [ -d "$subj/MNINonLinear/fsaverage_LR32k" ] && FS="yes" || FS="no"
-    [ -d "$subj/MNINonLinear/Results/month16-rest-1" ] && R1="yes" || R1="no"
-    [ -d "$subj/MNINonLinear/Results/month16-rest-2" ] && R2="yes" || R2="no"
-    [ -d "$subj/MNINonLinear/Results/month16-task-MSIT" ] && MSIT="yes" || MSIT="no"
+    [ -d "$subj/MNINonLinear/Results/month24-rest-1" ] && R1="yes" || R1="no"
+    [ -d "$subj/MNINonLinear/Results/month24-rest-2" ] && R2="yes" || R2="no"
+    [ -d "$subj/MNINonLinear/Results/month24-task-MSIT" ] && MSIT="yes" || MSIT="no"
 
     echo "$SUBJ_ID,$FS,$R1,$R2,$MSIT" >> $DATALIST
 done
